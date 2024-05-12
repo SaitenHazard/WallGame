a8b44c4d7102407d94d233018aa0a54c2a1f87b1
Leon Stier <ge35raz@tum.de>
2024-05-12 14:11:11 +0200

    Added interaction system
    Added E as Interaction Action
    Implement IInteractable to allow interaction on gameobject

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IInteractable
+{
+    public void Interact();
+}
+
+delegate void Interaction();
+
+public class Interactor : MonoBehaviour
+{
+    private Interaction _interact;    
+    // Start is called before the first frame update
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        IInteractable interactable = other.gameObject.GetComponent<IInteractable>();
+        if (interactable != null)
+        {
+            _interact = interactable.Interact;
+        }
+        
+        /*
+         * Possibly call to UI class to display Tooltip (Press E to Interact)
+         */
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _interact = () => { print("No interactable nearby"); };
+    }
+
+    void OnInteract()
+    {
+        _interact.Invoke();
+    }
+}
